5f2bcef238879d0fcb7b712356ad8affacbf5c24
Adriel <adriel.fsantos@outlook.com>
2022-05-18 11:08:45 -0300

    Implementa classe Rede

--- Rede.java
+++ Rede.java
@@ -1,33 +1,66 @@
 
+import java.util.ArrayList;
+
 /**
- * Escreva uma descrição da classe Rede aqui.
- * 
- * @author (seu nome) 
- * @version (um número da versão ou uma data)
+ * @author Adriel Faria dos Santos
+ * @version 18/05/2022
  */
-public class Rede
-{
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private int x;
+public class Rede {
 
-    /**
-     * Construtor para objetos da classe Rede
-     */
-    public Rede()
-    {
-        // inicializa variáveis de instância
-        x = 0;
+    private String nome;
+    private ArrayList<Mercado> mercados;
+
+    public Rede(String nome, ArrayList<Mercado> mercados) {
+        this.nome = nome;
+        this.mercados = mercados;
     }
 
-    /**
-     * Um exemplo de um método - substitua este comentário pelo seu próprio
-     * 
-     * @param  y   um exemplo de um parâmetro de método
-     * @return     a soma de x e y 
-     */
-    public int sampleMethod(int y)
-    {
-        // escreva seu código aqui
-        return x + y;
+    public Rede(String nome) {
+        this.nome = nome;
+        this.mercados = new ArrayList<Mercado>();
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public ArrayList<Mercado> getMercados() {
+        return mercados;
+    }
+
+    public void setMercados(ArrayList<Mercado> mercados) {
+        this.mercados = mercados;
+    }
+
+    public void addMercado(Mercado m) {
+        this.mercados.add(m);
+    }
+
+    public void totalVendas() {
+        double total = 0;
+        for (Mercado m : this.mercados) {
+            total += m.totalVendas();
+        }
+    }
+    
+    public ArrayList<Venda> vendas() {
+        ArrayList<Venda> vendas = new ArrayList<Venda>();
+        for (Mercado mercado : this.mercados) {
+            vendas.addAll(mercado.getVendas());
+        }
+        return vendas;
+    }
+    
+    public ArrayList<Venda> vendasMes(String mes) {
+        ArrayList<Venda> vendas = new ArrayList<Venda>();
+        for (Mercado mercado : this.mercados) {
+            vendas.addAll(mercado.vendasMes(mes));
+        }
+        return vendas;
+    }
+
 }
