69807c9c6b41284699aa6e1c91ecabf93cd6006d
Adriel <adriel.fsantos@outlook.com>
2022-05-18 11:01:44 -0300

    Implementa classe Venda

--- Venda.java
+++ Venda.java
@@ -1,33 +1,67 @@
+import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 /**
- * Escreva uma descrição da classe Venda aqui.
- * 
- * @author (seu nome) 
- * @version (um número da versão ou uma data)
+ * @author Adriel Faria dos Santos
+ * @version 18/05/2022
  */
-public class Venda
-{
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private int x;
+public class Venda {
+    private Date data;
+    private ArrayList<Item> itens;
+    private SimpleDateFormat formatador = new SimpleDateFormat("dd/MM/yyyy");
 
-    /**
-     * Construtor para objetos da classe Venda
-     */
-    public Venda()
-    {
-        // inicializa variáveis de instância
-        x = 0;
+    public Venda(String data, ArrayList<Item> itens) {
+        try {
+            this.data = formatador.parse(data);
+        } catch (java.text.ParseException pe) {
+            pe.printStackTrace();
+        }
+        this.itens = itens;
+    }
+    
+    public Venda(String data) {
+        try {
+            this.data = formatador.parse(data);
+        } catch (java.text.ParseException pe) {
+            pe.printStackTrace();
+        }
+        this.itens = new ArrayList<Item>();
     }
 
-    /**
-     * Um exemplo de um método - substitua este comentário pelo seu próprio
-     * 
-     * @param  y   um exemplo de um parâmetro de método
-     * @return     a soma de x e y 
-     */
-    public int sampleMethod(int y)
-    {
-        // escreva seu código aqui
-        return x + y;
+    public Date getData() {
+        return this.data;
+    }
+    
+    public String getDataString() {
+        return formatador.format(this.data);
+    }
+
+    public void setData(String data) {
+        try {
+            this.data = formatador.parse(data);
+        } catch (java.text.ParseException pe) {
+            pe.printStackTrace();
+        }
+    }
+
+    public ArrayList<Item> getItens() {
+        return itens;
+    }
+
+    public void setItens(ArrayList<Item> itens) {
+        this.itens = itens;
+    }
+
+    public void addItem(Item item) {
+        this.itens.add(item);
+    }
+    
+    public double total() {
+        double total = 0;
+        for (Item i : this.itens) {
+            total += i.total();
+        }
+        return total;
     }
 }
